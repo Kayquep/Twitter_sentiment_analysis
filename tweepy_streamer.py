bb9975168bec9e87ec9eb22b2754e84b0347f0c7
Kayque Pagiola Souza <kayquepagiola10@gmail.com>
2020-04-21 10:48:12 -0300

    Analyzing tweet data
    Creating a way to see the tweets and organazing in a data frame

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -5,14 +5,17 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 import twitter_credentials
-
+import numpy as np
+import pandas as pd
 
 class TwitterClient():
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
-
         self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
 
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
@@ -83,14 +86,31 @@ class TwitterListener(StreamListener):
             return False
         print(status)
 
+class TweetAnalyzer():
+    """
+    Functionality for analyzing and categorizing content from tweets
+    """
+
+    def tweets_to_dataframe(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        return df
 
 if __name__ == "__main__":
 
-    hash_tag_list = ["jair bolsonaro", "mandetta", "nelson teich"]
-    fetched_tweets_filename = "tweets.json"
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
 
-    twitter_client = TwitterClient('pycon')
-    print(twitter_client.get_user_timeline_tweets(1))
+    api = twitter_client.get_twitter_client_api()
 
-    #twitter_streamer = TwitterStreamer()
-    #twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    tweets = api.user_timeline(screen_name="jairbolsonaro", count=20)
+
+    df = tweet_analyzer.tweets_to_dataframe(tweets)
+
+    print(df.head(10))
